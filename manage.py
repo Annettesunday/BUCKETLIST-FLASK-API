51bf474b944f227eaac9e77ba9099b00aabe1057
Annettesunday <Anetsunday@gmail.com>
2017-08-28 18:49:47 +0300

    [Chore] Add test command, add postgres to travis

--- manage.py
+++ manage.py
@@ -1,3 +1,5 @@
+import unittest
+
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 from app import config
@@ -12,6 +14,16 @@ migrate = Migrate(app, db)
 manager = Manager(app)
 
 manager.add_command('db', MigrateCommand)
+
+# Usage: python manage.py test
+@manager.command
+def test():
+    """Runs the unit tests without test coverage."""
+    tests = unittest.TestLoader().discover('./tests', pattern='test*.py')
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+    if result.wasSuccessful():
+        return 0
+    return 1
 
 
 if __name__ == '__main__':
